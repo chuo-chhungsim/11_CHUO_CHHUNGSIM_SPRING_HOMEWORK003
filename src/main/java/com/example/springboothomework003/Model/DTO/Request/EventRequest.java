b29a1983d04c2e998deed6cfbd8e06e64039ad78
Simz <chhungsim.chuo@gmail.com>
2025-03-30 00:31:43 +0700

    fix validation part on event request

--- src/main/java/com/example/springboothomework003/Model/DTO/Request/EventRequest.java
+++ src/main/java/com/example/springboothomework003/Model/DTO/Request/EventRequest.java
@@ -2,6 +2,7 @@ package com.example.springboothomework003.Model.DTO.Request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventRequest {
+
 	@NotBlank(message = "Event name is mandatory")
+	@Size(max = 100, message = "Event name should not exceed 100 characters")
 	private String eventName;
+
+	@NotNull(message = "Event date is mandatory")
 	private Date eventDate;
+
+	@NotNull(message = "Venue ID is mandatory")
 	private Long venueId;
+
+	@NotNull(message = "Attendees ID list is mandatory")
+	@Size(min = 1, message = "There must be at least one attendee")
 	private List<Long> attendeesId;
 }
